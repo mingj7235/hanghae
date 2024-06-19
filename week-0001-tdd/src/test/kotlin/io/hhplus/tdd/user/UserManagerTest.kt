7f5390055970b8cf7a7dab04a62e858aff2e931a
mingj7235 <joshuara7235@gmail.com>
2024-06-19 20:55:52 +0900

    test : existUser 테스트 - 존재하는 userId 를 조회했을 때, true 를 리턴한다.

--- week-0001-tdd/src/test/kotlin/io/hhplus/tdd/user/UserManagerTest.kt
+++ week-0001-tdd/src/test/kotlin/io/hhplus/tdd/user/UserManagerTest.kt
@@ -2,13 +2,14 @@ package io.hhplus.tdd.user
 
 import io.hhplus.tdd.database.UserRepository
 import io.hhplus.tdd.point.UserManager
+import io.hhplus.tdd.user.data.User
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.mockito.BDDMockito.given
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
 class UserManagerTest {
@@ -28,12 +29,27 @@ class UserManagerTest {
     fun `notExistUserTest`() {
         // Given
         val notExistUserId = -1L
-        `given`(userRepository.findBy(notExistUserId)).willReturn(null)
 
         // When
+        `when`(userRepository.findBy(notExistUserId)).thenReturn(null)
         val existUser = userManager.existUser(notExistUserId)
 
         // Then
         assertThat(existUser).isEqualTo(false)
+    }
+
+    @Test
+    @DisplayName("[existUser] 존재하는 userId 를 조회했을 때, true 를 리턴한다. ")
+    fun `existUserTest`() {
+        // Given
+        val existUserId = 0L
+        val user = User(existUserId)
+
+        // When
+        `when`(userRepository.findBy(existUserId)).thenReturn(user)
+        val existUser = userManager.existUser(existUserId)
+
+        // Then
+        assertThat(existUser).isEqualTo(true)
     }
 }
