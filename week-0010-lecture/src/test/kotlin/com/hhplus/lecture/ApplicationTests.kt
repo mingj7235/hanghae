33bf13ac090decf7b9c1326fd226399b82828d8b
mingj7235 <joshuara7235@gmail.com>
2024-06-22 23:41:59 +0900

    feat : git init

---
+++ week-0010-lecture/src/test/kotlin/com/hhplus/lecture/ApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.hhplus.lecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
